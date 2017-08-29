006dad79138cf84a52dd14593d18df04a558d18d
xhrist14n <xhrist14n@gmail.com>
2017-08-29 13:05:53 -0500

    data install tomcat

---
+++ .bashrc
@@ -0,0 +1,12 @@
+# .bashrc 
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+if [ -f /etc/bashrc ]; then
+		. /etc/bashrc
+fi
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.i386
+export CATALINA_HOME=/opt/tomcat
+
--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ cd /opt
 wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M26/bin/apache-tomcat-9.0.0.M26.tar.gz -O tomcat.tar.gz
 tar -xvf tomcat.tar.gz 
 mv apache-tomcat-9.0.0.M26 tomcat
+cd /usr/lib/jvm/java-1.8.0-openjdk.i386
+pwd 
