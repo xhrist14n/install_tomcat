d1365c248b8f6597d330f04c04f2e53ebc090ae4
xhrist14n <xhrist14n@gmail.com>
2017-08-29 11:29:56 -0500

    data install tomcat

---
+++ git.sh
@@ -0,0 +1,4 @@
+git add . -f
+git commit -am "data install tomcat"
+git push origin master
+echo "data uploaded ... "
---
+++ install.sh
@@ -0,0 +1,5 @@
+yum update 
+yum install nano -y
+yum install wget -y
+yum install curl -y
+wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M26/bin/apache-tomcat-9.0.0.M26.tar.gz -O tomcat.tar.gz
