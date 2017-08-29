4e6483ba29c1f04bf551e7e50e66cc20431608e0
xhrist14n <xhrist14n@gmail.com>
2017-08-29 12:09:47 -0500

    data install app ...

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ yum update
 yum install nano -y
 yum install wget -y
 yum install curl -y
+yum install java-1.8.0-openjdk.i686 -y
 wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M26/bin/apache-tomcat-9.0.0.M26.tar.gz -O tomcat.tar.gz
