2a7ac972dbff7e07466d6ce48f72b1603297dbc2
xhrist14n <xhrist14n@gmail.com>
2017-08-29 12:45:04 -0500

    data install tomcat

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ yum install wget -y
 yum install curl -y
 yum install java-1.8.0-openjdk.i686 -y
 wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M26/bin/apache-tomcat-9.0.0.M26.tar.gz -O tomcat.tar.gz
+tar -xvf tomcat.tar.gz 
+mv apache-tomcat-9.0.0.M26 tomcat
